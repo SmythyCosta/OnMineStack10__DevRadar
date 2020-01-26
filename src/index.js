521e89593a8a80bb74f6a902f86e0ab89e3a0f0b
SmythyCosta <smythy.costa@hotmail.com>
2020-01-25 22:17:14 -0300

    conexao com mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
 
 const app = express();
+
+
+/**
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-jwyto.mongodb.net/test?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+ */
+
+mongoose.connect('mongodb://localhost/devradar')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...')
+);
 
 
 app.get('/', (req, res) => {
