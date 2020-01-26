b4f8f544ef04b4933c6ef9ff4488bc78478a2e8b
SmythyCosta <smythy.costa@hotmail.com>
2020-01-26 07:40:31 -0300

    services Search

---
+++ src/app/services/SearchService.js
@@ -0,0 +1,33 @@
+const Dev = require('../models/Dev');
+
+/**
+ * 
+ * buscar todos os devs em um raio 10km
+ * filtrar por technologias
+ * 
+ * @param {*} latitude 
+ * @param {*} longitude 
+ * @param {*} techs 
+ */
+const searchDevs = async (latitude, longitude, techs) => {
+
+    const techsLowerCase = techs.map(tech => tech.toLowerCase())
+  
+    const devs = await Dev.find({
+      techs: {
+        $in: techsLowerCase,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000000,
+        },
+      },
+    })
+    return devs;
+}
+
+module.exports = searchDevs;
